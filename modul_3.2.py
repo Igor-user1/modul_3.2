826ca503f31463716001bc84053e4a2e9e00ff56
Igor-user1 <xadeev.igor@icloud.com>
2024-08-23 22:06:08 +0300

    Add files via upload

---
+++ modul_3.2.py
@@ -0,0 +1,42 @@
+at_ = '@'
+end_1 = '.com'
+end_2 = '.ru'
+end_3 = '.net'
+
+
+def send_email(message, recipient, sender='university.help@gmail.com'):
+    global at_, end_1, end_2, end_3
+
+    if at_ not in recipient:
+        print('Невозможно отправить письмо с адреса ' + sender + ' на адрес ' + recipient)
+        return
+
+    if at_ not in sender:
+        print('Невозможно отправить письмо с адреса ' + sender + ' на адрес ' + recipient)
+        return
+
+    if end_1 or end_2 or end_3 in recipient:
+        print('')
+    else:
+        print('Невозможно отправить письмо с адреса ' + sender + ' на адрес ' + recipient)
+        return
+
+    if end_1 or end_2 or end_3 in sender:
+        print('')
+    else:
+        print('Невозможно отправить письмо с адреса ' + sender + ' на адрес ' + recipient)
+        return
+
+    if sender == recipient:
+        print('Нельзя отправить письмо самому себе!')
+        return
+
+    if sender == 'university.help@gmail.com':
+        print('Письмо успешно отправлено с адреса ' + sender + ' на адрес '+recipient)
+    else:
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ' + sender + ' на адрес ' + recipient)
+        return
+    return
+
+
+send_email('Привет!', 'university.help@gmail.com')
